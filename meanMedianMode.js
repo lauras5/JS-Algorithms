46c78c2626940ec129d6d8c2a33dad19b31c31df
Laura Salcido <idlaurasalcido@gmail.com>
2018-04-17 11:11:27 -0700

    js algorithm practice

---
+++ meanMedianMode.js
@@ -0,0 +1,16 @@
+// functional programming
+function getMean(array) {
+
+}
+
+function getMedian(array) {
+
+}
+
+function getMode(array) {
+
+}
+
+function meanMedianMode(array) {
+    // calls other 3 codes
+}
